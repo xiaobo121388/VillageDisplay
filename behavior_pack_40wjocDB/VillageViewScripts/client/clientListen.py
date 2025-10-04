7d2b603a3a8e6a5f98609e6975a399d0a916483d
xiaobo121388 <467359395@qq.com>
2025-10-04 20:00:11 +0800

    优化显示线条

--- behavior_pack_40wjocDB/VillageViewScripts/client/clientListen.py
+++ behavior_pack_40wjocDB/VillageViewScripts/client/clientListen.py
@@ -17,6 +17,7 @@ class ClientListen(BaseClient):
         self.framesList2= []
         self.framesList3 = []
         self.undoPlaceData = []
+        self.renderBox = []
         self.ListenForEvent(modConfig.MOD_NAME,modConfig.SERVER_SYSTEM,"RenderVillage",self,self.RenderVillage)
 
     @Listen("UiInitFinished")
@@ -43,109 +44,121 @@ class ClientListen(BaseClient):
         #销毁之前序列帧
         self.CloseRender()
         #村庄边界        
+        DrawingComp = CF.CreateDrawing(LID)
+        
         sizeX, sizeY, sizeZ = self.placeData['size']
-        offsetX, offsetY, offsetZ = sizeX / 2.0, sizeY / 2.0, sizeZ / 2.0
         sX, sY, sZ = self.placeData['placePos']
-        coverFramePos = [(sX + offsetX, sY + offsetY, sZ + 0.01), (sX + 0.01, sY + offsetY, sZ + offsetZ),
-                         (sX + offsetX, sY + 0.01, sZ + offsetZ), (sX + offsetX, sY + offsetY, sZ + sizeZ - 0.01),
-                         (sX + sizeX - 0.01, sY + offsetY, sZ + offsetZ),
-                         (sX + offsetX, sY + sizeY - 0.01, sZ + offsetZ)]
-        lineFramePos = [(sX + offsetX, sY, sZ), (sX + offsetX, sY + sizeY, sZ), (sX + offsetX, sY + sizeY, sZ + sizeZ),
-                        (sX + offsetX, sY, sZ + sizeZ),
-                        (sX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ + sizeZ),
-                        (sX, sY + offsetY, sZ + sizeZ),
-                        (sX, sY, sZ + offsetZ), (sX + sizeX, sY, sZ + offsetZ), (sX + sizeX, sY + sizeY, sZ + offsetZ),
-                        (sX, sY + sizeY, sZ + offsetZ)]
-        coverFrameRotate = [(0, 0, 0), (0, 90, 0), (90, 0, 0),
-                            (0, 0, 0), (0, 90, 0), (90, 0, 0)]
-        lineFrameRot = [(0, 0, 90), (0, 0, 0), (90, 0, 0)]
-        coverFrameScale = [(offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1),
-                           (offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1)]
-        lineFrameScale = [(1, sizeX, 1), (1, sizeY, 1), (1, sizeZ, 1)]
-        # self.framesList1 = [self.CreateEngineSfxFromEditor(
-        #     'effects/building_frame.json', coverFramePos[i], coverFrameRotate[i], coverFrameScale[i]) for i in range(6)]
-        self.framesList1 += [self.CreateEngineSfxFromEditor(
-            'effects/building_frame_line.json', lineFramePos[i], lineFrameRot[i // 4], lineFrameScale[i // 4]) for i in
-            range(12)]
-        for frame in self.framesList1:
-            frameAniControlComp = CF.CreateFrameAniControl(frame)
-            frameAniControlComp.SetMixColor((1, 0, 0, 0.8))
-            frameAniControlComp.SetDeepTest(False)
-            frameAniControlComp.Play()
+        shape = DrawingComp.AddBoxShape((sX, sY, sZ),(sizeX,sizeY,sizeZ),(1,0,0))
+        self.renderBox.append(shape)
+        # offsetX, offsetY, offsetZ = sizeX / 2.0, sizeY / 2.0, sizeZ / 2.0
+        
+        # coverFramePos = [(sX + offsetX, sY + offsetY, sZ + 0.01), (sX + 0.01, sY + offsetY, sZ + offsetZ),
+        #                  (sX + offsetX, sY + 0.01, sZ + offsetZ), (sX + offsetX, sY + offsetY, sZ + sizeZ - 0.01),
+        #                  (sX + sizeX - 0.01, sY + offsetY, sZ + offsetZ),
+        #                  (sX + offsetX, sY + sizeY - 0.01, sZ + offsetZ)]
+        # lineFramePos = [(sX + offsetX, sY, sZ), (sX + offsetX, sY + sizeY, sZ), (sX + offsetX, sY + sizeY, sZ + sizeZ),
+        #                 (sX + offsetX, sY, sZ + sizeZ),
+        #                 (sX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ + sizeZ),
+        #                 (sX, sY + offsetY, sZ + sizeZ),
+        #                 (sX, sY, sZ + offsetZ), (sX + sizeX, sY, sZ + offsetZ), (sX + sizeX, sY + sizeY, sZ + offsetZ),
+        #                 (sX, sY + sizeY, sZ + offsetZ)]
+        # coverFrameRotate = [(0, 0, 0), (0, 90, 0), (90, 0, 0),
+        #                     (0, 0, 0), (0, 90, 0), (90, 0, 0)]
+        # lineFrameRot = [(0, 0, 90), (0, 0, 0), (90, 0, 0)]
+        # coverFrameScale = [(offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1),
+        #                    (offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1)]
+        # lineFrameScale = [(1, sizeX, 1), (1, sizeY, 1), (1, sizeZ, 1)]
+        # # self.framesList1 = [self.CreateEngineSfxFromEditor(
+        # #     'effects/building_frame.json', coverFramePos[i], coverFrameRotate[i], coverFrameScale[i]) for i in range(6)]
+        # self.framesList1 += [self.CreateEngineSfxFromEditor(
+        #     'effects/building_frame_line.json', lineFramePos[i], lineFrameRot[i // 4], lineFrameScale[i // 4]) for i in
+        #     range(12)]
+        # for frame in self.framesList1:
+        #     frameAniControlComp = CF.CreateFrameAniControl(frame)
+        #     frameAniControlComp.SetMixColor((1, 0, 0, 0.8))
+        #     frameAniControlComp.SetDeepTest(False)
+        #     frameAniControlComp.Play()
         #村庄中心
-        sizeX, sizeY, sizeZ = 1,1,1
-        offsetX, offsetY, offsetZ = sizeX / 2.0, sizeY / 2.0, sizeZ / 2.0
         sX, sY, sZ = self.placeData['centerPos']
-        coverFramePos = [(sX + offsetX, sY + offsetY, sZ + 0.01), (sX + 0.01, sY + offsetY, sZ + offsetZ),
-                         (sX + offsetX, sY + 0.01, sZ + offsetZ), (sX + offsetX, sY + offsetY, sZ + sizeZ - 0.01),
-                         (sX + sizeX - 0.01, sY + offsetY, sZ + offsetZ),
-                         (sX + offsetX, sY + sizeY - 0.01, sZ + offsetZ)]
-        lineFramePos = [(sX + offsetX, sY, sZ), (sX + offsetX, sY + sizeY, sZ), (sX + offsetX, sY + sizeY, sZ + sizeZ),
-                        (sX + offsetX, sY, sZ + sizeZ),
-                        (sX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ + sizeZ),
-                        (sX, sY + offsetY, sZ + sizeZ),
-                        (sX, sY, sZ + offsetZ), (sX + sizeX, sY, sZ + offsetZ), (sX + sizeX, sY + sizeY, sZ + offsetZ),
-                        (sX, sY + sizeY, sZ + offsetZ)]
-        coverFrameRotate = [(0, 0, 0), (0, 90, 0), (90, 0, 0),
-                            (0, 0, 0), (0, 90, 0), (90, 0, 0)]
-        lineFrameRot = [(0, 0, 90), (0, 0, 0), (90, 0, 0)]
-        coverFrameScale = [(offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1),
-                           (offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1)]
-        lineFrameScale = [(1, sizeX, 1), (1, sizeY, 1), (1, sizeZ, 1)]
-        self.framesList2 += [self.CreateEngineSfxFromEditor(
-            'effects/building_frame.json', coverFramePos[i], coverFrameRotate[i], coverFrameScale[i]) for i in range(6)]
-        self.framesList2 += [self.CreateEngineSfxFromEditor(
-            'effects/building_frame_line.json', lineFramePos[i], lineFrameRot[i // 4], lineFrameScale[i // 4]) for i in
-            range(12)]
-        for frame in self.framesList2:
-            frameAniControlComp = CF.CreateFrameAniControl(frame)
-            frameAniControlComp.SetMixColor((1, 1, 0, 0.12))
-            frameAniControlComp.SetDeepTest(False)
-            frameAniControlComp.Play()
-        #铁傀儡
-        sizeX, sizeY, sizeZ = 17,13,17
-        offsetX, offsetY, offsetZ = sizeX / 2.0, sizeY / 2.0, sizeZ / 2.0
-        sX, sY, sZ = self.placeData['ironPos']
-        coverFramePos = [(sX + offsetX, sY + offsetY, sZ + 0.01), (sX + 0.01, sY + offsetY, sZ + offsetZ),
-                         (sX + offsetX, sY + 0.01, sZ + offsetZ), (sX + offsetX, sY + offsetY, sZ + sizeZ - 0.01),
-                         (sX + sizeX - 0.01, sY + offsetY, sZ + offsetZ),
-                         (sX + offsetX, sY + sizeY - 0.01, sZ + offsetZ)]
-        lineFramePos = [(sX + offsetX, sY, sZ), (sX + offsetX, sY + sizeY, sZ), (sX + offsetX, sY + sizeY, sZ + sizeZ),
-                        (sX + offsetX, sY, sZ + sizeZ),
-                        (sX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ + sizeZ),
-                        (sX, sY + offsetY, sZ + sizeZ),
-                        (sX, sY, sZ + offsetZ), (sX + sizeX, sY, sZ + offsetZ), (sX + sizeX, sY + sizeY, sZ + offsetZ),
-                        (sX, sY + sizeY, sZ + offsetZ)]
-        coverFrameRotate = [(0, 0, 0), (0, 90, 0), (90, 0, 0),
-                            (0, 0, 0), (0, 90, 0), (90, 0, 0)]
-        lineFrameRot = [(0, 0, 90), (0, 0, 0), (90, 0, 0)]
-        coverFrameScale = [(offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1),
-                           (offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1)]
-        lineFrameScale = [(1, sizeX, 1), (1, sizeY, 1), (1, sizeZ, 1)]
-        # self.framesList3 += [self.CreateEngineSfxFromEditor(
+        shape = DrawingComp.AddBoxShape((sX, sY, sZ),(1,1,1),(1,1,0))
+        self.renderBox.append(shape)
+        # sizeX, sizeY, sizeZ = 1,1,1
+        # offsetX, offsetY, offsetZ = sizeX / 2.0, sizeY / 2.0, sizeZ / 2.0
+        # sX, sY, sZ = self.placeData['centerPos']
+        # coverFramePos = [(sX + offsetX, sY + offsetY, sZ + 0.01), (sX + 0.01, sY + offsetY, sZ + offsetZ),
+        #                  (sX + offsetX, sY + 0.01, sZ + offsetZ), (sX + offsetX, sY + offsetY, sZ + sizeZ - 0.01),
+        #                  (sX + sizeX - 0.01, sY + offsetY, sZ + offsetZ),
+        #                  (sX + offsetX, sY + sizeY - 0.01, sZ + offsetZ)]
+        # lineFramePos = [(sX + offsetX, sY, sZ), (sX + offsetX, sY + sizeY, sZ), (sX + offsetX, sY + sizeY, sZ + sizeZ),
+        #                 (sX + offsetX, sY, sZ + sizeZ),
+        #                 (sX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ + sizeZ),
+        #                 (sX, sY + offsetY, sZ + sizeZ),
+        #                 (sX, sY, sZ + offsetZ), (sX + sizeX, sY, sZ + offsetZ), (sX + sizeX, sY + sizeY, sZ + offsetZ),
+        #                 (sX, sY + sizeY, sZ + offsetZ)]
+        # coverFrameRotate = [(0, 0, 0), (0, 90, 0), (90, 0, 0),
+        #                     (0, 0, 0), (0, 90, 0), (90, 0, 0)]
+        # lineFrameRot = [(0, 0, 90), (0, 0, 0), (90, 0, 0)]
+        # coverFrameScale = [(offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1),
+        #                    (offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1)]
+        # lineFrameScale = [(1, sizeX, 1), (1, sizeY, 1), (1, sizeZ, 1)]
+        # self.framesList2 += [self.CreateEngineSfxFromEditor(
         #     'effects/building_frame.json', coverFramePos[i], coverFrameRotate[i], coverFrameScale[i]) for i in range(6)]
-        self.framesList3 += [self.CreateEngineSfxFromEditor(
-            'effects/building_frame_line.json', lineFramePos[i], lineFrameRot[i // 4], lineFrameScale[i // 4]) for i in
-            range(12)]
-        for frame in self.framesList3:
-            frameAniControlComp = CF.CreateFrameAniControl(frame)
-            frameAniControlComp.SetMixColor((0, 0, 1, 0.8))
-            frameAniControlComp.SetDeepTest(False)
-            frameAniControlComp.Play()
+        # self.framesList2 += [self.CreateEngineSfxFromEditor(
+        #     'effects/building_frame_line.json', lineFramePos[i], lineFrameRot[i // 4], lineFrameScale[i // 4]) for i in
+        #     range(12)]
+        # for frame in self.framesList2:
+        #     frameAniControlComp = CF.CreateFrameAniControl(frame)
+        #     frameAniControlComp.SetMixColor((1, 1, 0, 0.12))
+        #     frameAniControlComp.SetDeepTest(False)
+        #     frameAniControlComp.Play()
+        #铁傀儡
+        sX, sY, sZ = self.placeData['ironPos']
+        shape = DrawingComp.AddBoxShape((sX, sY, sZ),(17,13,17),(0,0,1))
+        self.renderBox.append(shape)
+        # offsetX, offsetY, offsetZ = sizeX / 2.0, sizeY / 2.0, sizeZ / 2.0
+        # sX, sY, sZ = self.placeData['ironPos']
+        # coverFramePos = [(sX + offsetX, sY + offsetY, sZ + 0.01), (sX + 0.01, sY + offsetY, sZ + offsetZ),
+        #                  (sX + offsetX, sY + 0.01, sZ + offsetZ), (sX + offsetX, sY + offsetY, sZ + sizeZ - 0.01),
+        #                  (sX + sizeX - 0.01, sY + offsetY, sZ + offsetZ),
+        #                  (sX + offsetX, sY + sizeY - 0.01, sZ + offsetZ)]
+        # lineFramePos = [(sX + offsetX, sY, sZ), (sX + offsetX, sY + sizeY, sZ), (sX + offsetX, sY + sizeY, sZ + sizeZ),
+        #                 (sX + offsetX, sY, sZ + sizeZ),
+        #                 (sX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ), (sX + sizeX, sY + offsetY, sZ + sizeZ),
+        #                 (sX, sY + offsetY, sZ + sizeZ),
+        #                 (sX, sY, sZ + offsetZ), (sX + sizeX, sY, sZ + offsetZ), (sX + sizeX, sY + sizeY, sZ + offsetZ),
+        #                 (sX, sY + sizeY, sZ + offsetZ)]
+        # coverFrameRotate = [(0, 0, 0), (0, 90, 0), (90, 0, 0),
+        #                     (0, 0, 0), (0, 90, 0), (90, 0, 0)]
+        # lineFrameRot = [(0, 0, 90), (0, 0, 0), (90, 0, 0)]
+        # coverFrameScale = [(offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1),
+        #                    (offsetX, offsetY, 1), (offsetZ, offsetY, 1), (offsetX, offsetZ, 1)]
+        # lineFrameScale = [(1, sizeX, 1), (1, sizeY, 1), (1, sizeZ, 1)]
+        # # self.framesList3 += [self.CreateEngineSfxFromEditor(
+        # #     'effects/building_frame.json', coverFramePos[i], coverFrameRotate[i], coverFrameScale[i]) for i in range(6)]
+        # self.framesList3 += [self.CreateEngineSfxFromEditor(
+        #     'effects/building_frame_line.json', lineFramePos[i], lineFrameRot[i // 4], lineFrameScale[i // 4]) for i in
+        #     range(12)]
+        # for frame in self.framesList3:
+        #     frameAniControlComp = CF.CreateFrameAniControl(frame)
+        #     frameAniControlComp.SetMixColor((0, 0, 1, 0.8))
+        #     frameAniControlComp.SetDeepTest(False)
+        #     frameAniControlComp.Play()
     def CloseRender(self,args=None):
                 #销毁之前序列帧
-        if self.framesList1:
-            for frame in self.framesList1:
-                self.DestroyEntity(frame)
-                self.framesList1 = []
-        if self.framesList2:
-            for frame in self.framesList2:
-                self.DestroyEntity(frame)
-                self.framesList2 = []
-        if self.framesList3:
-            for frame in self.framesList3:
-                self.DestroyEntity(frame)
-                self.framesList3 = []
+        # if self.framesList1:
+        #     for frame in self.framesList1:
+        #         self.DestroyEntity(frame)
+        #         self.framesList1 = []
+        # if self.framesList2:
+        #     for frame in self.framesList2:
+        #         self.DestroyEntity(frame)
+        #         self.framesList2 = []
+        # if self.framesList3:
+        #     for frame in self.framesList3:
+        #         self.DestroyEntity(frame)
+        #         self.framesList3 = []
+        for shape in self.renderBox:
+            shape.Remove()
     def OnVillageCommand(self,args):
         self.UINode = clientApi.PushScreen(modConfig.MOD_NAME,"VillageView")
     def CheckVillage(self,args):
